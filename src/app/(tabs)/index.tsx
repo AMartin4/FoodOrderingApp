e5b499e201b66b88d8c2e1a943e70e4979e0a85b
amartin4 <aamartin20@students.desu.edu>
2024-02-19 18:09:37 -0500

    Add existing files to git

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,41 +1,12 @@
-import { StyleSheet, Image } from 'react-native';
-import Colors from '../../constants/Colors'
-import products from '@/assets/data/products';
+import { View } from 'react-native';
+import products from '../../../assets/data/products';
+import ProductListItem from '../../components/ProductListItem';
 
-const product = products[1]
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
-
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
-  );
+export default function MenuScreen() {
+  return (<View>
+    <ProductListItem product={products[0]} />
+    <ProductListItem product={products[5]} />
+  </View>);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  },
-
-  title: {
-    fontSize: 18, 
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold'
-  },
-
-});
